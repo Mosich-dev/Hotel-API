92821e606e2b3d09164c3ccaa482ff9bf8ea86df
Mosich-dev <mosich.dev@gmail.com>
2023-12-03 22:18:36 +0330

    adding mongoDB and trying it out

--- main.go
+++ main.go
@@ -1,12 +1,46 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
 	"github.com/Mosich-dev/Hotel-API/API"
+	"github.com/Mosich-dev/Hotel-API/types"
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+)
+
+const (
+	dburi    = "mongodb://localhost:27017"
+	dbname   = "hotel-api"
+	userColl = "users"
 )
 
 func main() {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dburi))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(client)
+	ctx := context.Background()
+	coll := client.Database(dbname).Collection(userColl)
+	user := types.User{
+		FirstName: "mosi",
+		LastName:  "che",
+	}
+	_, err = coll.InsertOne(ctx, user)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var mosi types.User
+	err = coll.FindOne(ctx, bson.M{}).Decode(&mosi)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(mosi)
 	app := fiber.New()
 	listenAddr := flag.String("listenaddr", ":5000", "The Listen address of the API server")
 	flag.Parse()
